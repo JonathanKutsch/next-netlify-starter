bc39a73e08f23e85f2bb7ab8ed65e1e7434d4d29
Jonathan Kutsch <jonkutschedu@gmail.com>
2023-11-01 15:22:59 -0500

    add table

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,62 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Table Example</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          {/* Get started by editing <code>pages/index.js</code> */}
-          This is a parsing test website.
-        </p>
+        <table>
+          <thead>
+            <tr>
+              <th>Header 1</th>
+              <th>Header 2</th>
+              <th>Header 3</th>
+              <th>Header 4</th>
+              <th>Header 5</th>
+              <th>Header 6</th>
+              <th>Header 7</th>
+              <th>Header 8</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Row 1 Data 1</td>
+              <td>Row 1 Data 2</td>
+              <td>Row 1 Data 3</td>
+              <td>Row 1 Data 4</td>
+              <td>Row 1 Data 5</td>
+              <td>Row 1 Data 6</td>
+              <td>Row 1 Data 7</td>
+              <td>Row 1 Data 8</td>
+            </tr>
+            <tr>
+              <td>Row 2 Data 1</td>
+              <td>Row 2 Data 2</td>
+              <td>Row 2 Data 3</td>
+              <td>Row 2 Data 4</td>
+              <td>Row 2 Data 5</td>
+              <td>Row 2 Data 6</td>
+              <td>Row 2 Data 7</td>
+              <td>Row 2 Data 8</td>
+            </tr>
+            <tr>
+              <td>Row 3 Data 1</td>
+              <td>Row 3 Data 2</td>
+              <td>Row 3 Data 3</td>
+              <td>Row 3 Data 4</td>
+              <td>Row 3 Data 5</td>
+              <td>Row 3 Data 6</td>
+              <td>Row 3 Data 7</td>
+              <td>Row 3 Data 8</td>
+            </tr>
+          </tbody>
+        </table>
       </main>
-
-      <Footer />
     </div>
   )
 }
