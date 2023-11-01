a40db7302952e740293b0fe88f755bda903290b3
Jonathan Kutsch <jonkutschedu@gmail.com>
2023-11-01 15:18:09 -0500

    test commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,8 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          {/* Get started by editing <code>pages/index.js</code> */}
+          This is a parsing test website.
         </p>
       </main>
 
